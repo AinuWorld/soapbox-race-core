b82672b05442c72e1484235f947ae8afc3f8ee42
Hazuki Onohara <aoba@outlook.co.th>
2021-10-07 12:40:35 +0700

    Revert MPV

--- src/main/java/com/soapboxrace/core/auth/verifiers/ModernPasswordVerifier.java
+++ src/main/java/com/soapboxrace/core/auth/verifiers/ModernPasswordVerifier.java
@@ -11,7 +11,6 @@ import com.google.common.hash.Hashing;
 import com.soapboxrace.core.auth.AuthUtil;
 import com.soapboxrace.core.bo.Argon2BO;
 import com.soapboxrace.core.jpa.UserEntity;
-import org.apache.commons.codec.digest.DigestUtils;
 
 public class ModernPasswordVerifier implements PasswordVerifier {
     private Argon2BO argon2;
@@ -34,14 +33,13 @@ public class ModernPasswordVerifier implements PasswordVerifier {
         if (dbHash.length() == 40) {
             // DB has legacy hash, verify
             @SuppressWarnings("deprecation")
-            //String legacyHash = Hashing.sha1().hashString(password, Charsets.UTF_8).toString();
-            String legacyHash = DigestUtils.sha1Hex(password);
+            String legacyHash = Hashing.sha1().hashString(password, Charsets.UTF_8).toString();
             if (!AuthUtil.stringsEqual(dbHash, legacyHash)) {
                 return false;
             }
             needsRehash = true;
         } else {
-            if (!argon2.verify(Hashing.sha1().hashString(password, Charsets.UTF_8).toString(), dbHash)) {
+            if (!argon2.verify(password, dbHash)) {
                 return false;
             }
             needsRehash = argon2.needsRehash(dbHash);
